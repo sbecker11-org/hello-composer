074168c721f2cf0733b212765ffada5b96f5728f
Shawn Becker <shawn.becker@datalaboratory.com>
2021-03-31 09:48:28 -0600

    added README file

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '6866541c64ad190dd5090be70a858f72a3c41271',
+    'name' => 'sbecker11/hello-composer',
+  ),
+  'versions' => 
+  array (
+    'sbecker11/hello-composer' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6866541c64ad190dd5090be70a858f72a3c41271',
+    ),
+  ),
+);
